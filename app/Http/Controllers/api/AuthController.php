5d3c090e0db4ad8025371c06b1f40c05326e563f
blacczen <adeyinkaryusuf@gmail.com>
2023-09-18 09:21:27 +0100

    migrations

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -2,19 +2,23 @@
 
 namespace App\Http\Controllers\api;
 
-use App\Events\SignUpEvent;
 use App\Models\Artiste;
 use Illuminate\Http\Request;
 use App\Traits\ResponseTrait;
 use App\Services\MediaService;
 use Illuminate\Http\JsonResponse;
-use App\Models\{Cart, User, Producer, };
+// use Illuminate\Support\Facades\DB;
 use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignUpRequest;
-use Illuminate\Support\Facades\{DB, Auth};
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Resources\UserResources;
+use App\Models\{Cart, User, Producer };
+
+
+
 
 class AuthController extends Controller
 {
@@ -49,7 +53,6 @@ class AuthController extends Controller
             Cart::create(['user_id' => $user->id, 'items' => []]);
 
         }
-        event(new SignUpEvent ($user));
        
         return $this->successResponse('User created successfully', [
             'user' => new UserResources($user)
@@ -68,11 +71,62 @@ class AuthController extends Controller
         $token = $user->generateToken();
         $token = $user->createToken($user->email, [$user->user_type])->accessToken;
 
+        $userAgent = $request->header('User-Agent');
+        $userIP = $request->ip();
+        Log::info('User agent details: ' . $userAgent);
+        Log::info('User IP: ' . $userIP);
+
+
         return $this->successResponse('User logged in successfully', [
             'token' => $token,
             'user' => new UserResources($user)
         ]);
     }
+
+
+    // public function forgetPassword(ForgotPasswordRequest $request)
+    // {
+    //     // Delete all old codes that the user sent before.
+    //     ResetCodePassword::where('email', $request->email)->delete();
+
+    //     // Generate random code
+    //     $data['email'] = $request->email;
+    //     $data['Token'] = rand(100, 999); // Updated range for 3-digit numbers
+    //     $data['created'] = now();
+
+    //     $code = ResetCodePassword::create($data);
+
+    //     return response(['message' => trans('passwords.sent')], 200);
+    // }
+
+    // public function passwordReset(PasswordResetRequest $request)
+    // {
+    //     $user = User::where('email', $request->email)->first();
+
+    //     if (!$user) {
+    //         return response()->json(['message' => 'User not found'], 404);
+    //     }
+
+    //     // Validate the password using Laravel's validation rules.
+    //     $validatedData = $request->validate([
+    //         'password' => ['required', 'string', 'min:8'], // Add your desired validation rules
+    //     ]);
+
+    //     $user->fill([
+    //         'password' => Hash::make($validatedData['password']),
+    //     ]);
+    //     $user->save();
+
+    //     DB::table('resetcodepassword')
+    //         ->where('email', $user->email)
+    //         ->delete();
+
+    //     return response()->json([
+    //         'message' => 'Password reset successful',
+    //         'data' => [$user],
+    //     ]);
+    // }
+
 
     public function signout(Request $request): JsonResponse
     {
